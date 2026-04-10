d87d287f71e0cb02f91167903be5f1e8cc2404f1
aksel <aksel.vaaler@ntnu.no>
2026-04-10 15:42:18 +0200

    noe små greier

--- masteroppgave/simulering_1.py
+++ masteroppgave/simulering_1.py
@@ -2,18 +2,20 @@ from roboeval.action_modes import JointPositionActionMode
 from roboeval.demonstrations.demo_player import DemoPlayer
 from roboeval.demonstrations.demo_store import DemoStore
 from roboeval.demonstrations.utils import Metadata
-from roboeval.envs.stack_books import PickSingleBookFromTablePositionAndOrientation
+from roboeval.envs.stack_books import PickSingleBookFromTablePositionAndOrientation, PickSingleBookFromTableOrientation
 from roboeval.robots.configs.panda import BimanualPanda, SinglePanda
 from roboeval.utils.observation_config import ObservationConfig, CameraConfig
 from roboeval.envs.lift_pot import LiftPot, LiftPotPositionAndOrientation
 from roboeval.envs.stack_books import StackSingleBookShelf
-from roboeval.envs.manipulation import StackTwoBlocksPositionAndOrientation
+from roboeval.envs.manipulation import StackTwoBlocksPositionAndOrientation, CubeHandoverOrientation
 from roboeval.envs.pack_objects import PackBoxPosition
+from roboeval.envs.rotate_utility_objects import RotateValvePosition
+from roboeval.envs.lift_tray import DragOverAndLiftTray, LiftTrayPosition
 #BANE 
 #~/.roboeval/roboeval_demos/1.0.0/BimanualPanda/LiftPot/JointPositionActionMode_floating_absolute_joint
 
 # Create environment with camera observations
-env = LiftPotPositionAndOrientation(
+env = PackBoxPosition(
     action_mode=JointPositionActionMode(floating_base=True, absolute=True, floating_dofs=[]),
     render_mode="human", #rgb_array
     control_frequency=20,
@@ -49,16 +51,20 @@ env = LiftPotPositionAndOrientation(
 
 #Get demonstrations from DemoStore
 metadata = Metadata.from_env(env)
-all_demos = DemoStore().get_demos(metadata, amount=102, frequency=20)
+all_demos = DemoStore().get_demos(metadata, amount=115, frequency=20)
 
 # Filter to only successful demos (at least one step with reward > 0)
 successful_demos = [
     demo for demo in all_demos
     if sum(step.reward for step in demo.timesteps) > 0
 ]
-demos = successful_demos[:100]
+demos = successful_demos[:110]
 print(f"Collected {len(all_demos)} demos, {len(successful_demos)} successful, using {len(demos)}")
 
+demo_test = demos[31]
 # Replay demonstrations
-for demo in demos:
-    DemoPlayer().replay_in_env(demo, env, demo_frequency=20)
+
+#for demo in demos:
+#    DemoPlayer().replay_in_env(demo, env, demo_frequency=20)
+
+DemoPlayer().replay_in_env(demo_test, env, demo_frequency=20)
